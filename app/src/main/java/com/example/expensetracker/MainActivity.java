c94a9d567c0351d881dc92420aaa60ff6b6362be
naikaj sevak <naikajsevak98@gmail.com>
2025-05-14 17:08:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/expensetracker/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.expensetracker;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.expensetracker.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
